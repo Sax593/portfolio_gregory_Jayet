78d0cf5a80ad3e9dd1cbeabf3a57a27ebb083e1f
sax59 <waquetv@gmail.com>
2022-11-29 16:39:24 +0100

    Create route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import Home from "@pages/Home/Home";
+import Project from "@pages/Project/Project";
+import Skill from "@pages/Skill/Skill";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <p>Coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skill />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
